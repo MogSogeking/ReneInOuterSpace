df961b4cc6a9731505cfb58ddfd8069fedb591f9
Vincent Poesmann <vincent.poesmann@gmail.com>
2018-08-11 13:32:39 +0200

    Add spawn timer

--- main.js
+++ main.js
@@ -20,6 +20,9 @@ const game = new Phaser.Game(config);
 let keys;
 let rene;
 let meteor;
+let delay = 5000;
+let spawnEvent;
+let spawnCount = 1;
 
 function preload() {
   this.load.image('rene', 'assets/rene.png');
@@ -27,14 +30,20 @@ function preload() {
 }
 
 function create() {
+  keys = this.input.keyboard.addKeys('Z,Q,S,D,SPACE');
+
   rene = this.physics.add.image(32, 300, 'rene');
   setReneConfig();
-  keys = this.input.keyboard.addKeys('Z,Q,S,D,SPACE');
-  meteor = this.physics.add.image(600, 300, 'meteor').setBounce(0.5, 0.5).setMass(10);
-  this.physics.add.collider(rene, meteor);
 
+  spawnEvent = this.time.addEvent({
+    delay, callback: spawnObjects, callbackScope: this
+  });
+
+  meteor = this.physics.add.image(600, 300, 'meteor').setBounce(0.5, 0.5).setMass(10).setCircle(32);
   meteor.setAngularVelocity(20);
   meteor.setVelocity(-200, 0);
+
+  this.physics.add.collider(rene, meteor);
 }
 
 function update() {
@@ -46,7 +55,9 @@ function setReneConfig() {
     .setMass(1)
     .setDrag(0.98, 0.98)
     .setAngularVelocity(120)
-    .setMaxVelocity(300, 300);
+    .setMaxVelocity(300, 300)
+    .setCircle(32)
+    .setCollideWorldBounds(true);
   rene.body.useDamping = true;
 }
 
@@ -58,4 +69,13 @@ function movingRene() {
   } else {
     rene.setAngularVelocity(120);
   }
+}
+
+function spawnObjects() {
+  delay -= 1000 / Math.pow(spawnCount, 1.2);
+  spawnCount++;
+  console.log(delay);
+  spawnEvent = this.time.addEvent({
+    delay, callback: spawnObjects, callbackScope: this
+  });
 }
