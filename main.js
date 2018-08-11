23dd4453062936d673ad2fda6d69f1e934d5c7b9
Vincent Poesmann <vincent.poesmann@gmail.com>
2018-08-11 12:23:41 +0200

    Add rene's movement add first meteor

--- main.js
+++ main.js
@@ -1,45 +1,50 @@
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create
-    }
-};
-
-const game = new Phaser.Game(config);
-
 let keys;
+let rene;
+let meteor;
 
 function preload() {
-    this.load.image('character', 'assets/character.png');
+  this.load.image('rene', 'assets/rene.png');
+  this.load.image('meteor', 'assets/meteor.png');
 }
 
 function create() {
-
-    keys = this.input.keyboard.addKeys('Z,Q,S,D');
-    this.add.image(400, 300, 'sky');
-
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+  keys = this.input.keyboard.addKeys('Z,Q,S,D');
+  rene = this.physics.add.image(32, 300, 'rene').setBounce(1, 1).setMass(1).setDrag(0.98, 0.98);
+  meteor = this.physics.add.image(600, 300, 'meteor').setBounce(0.5, 0.5).setMass(10);
+  this.physics.add.collider(rene, meteor);
+  rene.body.useDamping = true;
+  rene.setAngularVelocity(120);
+  meteor.setAngularVelocity(20);
+  meteor.setVelocity(-200, 0);
+  rene.setMaxVelocity(300, 300);
 }
+
+function update() {
+  console.log(rene.body.acceleration);
+  rene.setAccelerationX((keys.D.isDown - keys.Q.isDown) * 1000);
+  rene.setAccelerationY((keys.S.isDown - keys.Z.isDown) * 1000);
+  if (keys.S.isDown || keys.D.isDown || keys.Q.isDown || keys.Z.isDown) {
+    rene.setAngularVelocity(360);
+  } else {
+    rene.setAngularVelocity(120);
+  }
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+    },
+  },
+  scene: {
+    preload,
+    create,
+    update,
+  }
+};
+
+const game = new Phaser.Game(config);
