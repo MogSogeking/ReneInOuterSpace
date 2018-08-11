99830f157551324eeb5f31823b3175baf4ba0635
Vincent Poesmann <vincent.poesmann@gmail.com>
2018-08-11 12:43:25 +0200

    Move code in functions

--- main.js
+++ main.js
@@ -1,35 +1,3 @@
-let keys;
-let rene;
-let meteor;
-
-function preload() {
-  this.load.image('rene', 'assets/rene.png');
-  this.load.image('meteor', 'assets/meteor.png');
-}
-
-function create() {
-  keys = this.input.keyboard.addKeys('Z,Q,S,D');
-  rene = this.physics.add.image(32, 300, 'rene').setBounce(1, 1).setMass(1).setDrag(0.98, 0.98);
-  meteor = this.physics.add.image(600, 300, 'meteor').setBounce(0.5, 0.5).setMass(10);
-  this.physics.add.collider(rene, meteor);
-  rene.body.useDamping = true;
-  rene.setAngularVelocity(120);
-  meteor.setAngularVelocity(20);
-  meteor.setVelocity(-200, 0);
-  rene.setMaxVelocity(300, 300);
-}
-
-function update() {
-  console.log(rene.body.acceleration);
-  rene.setAccelerationX((keys.D.isDown - keys.Q.isDown) * 1000);
-  rene.setAccelerationY((keys.S.isDown - keys.Z.isDown) * 1000);
-  if (keys.S.isDown || keys.D.isDown || keys.Q.isDown || keys.Z.isDown) {
-    rene.setAngularVelocity(360);
-  } else {
-    rene.setAngularVelocity(120);
-  }
-}
-
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -48,3 +16,46 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+
+let keys;
+let rene;
+let meteor;
+
+function preload() {
+  this.load.image('rene', 'assets/rene.png');
+  this.load.image('meteor', 'assets/meteor.png');
+}
+
+function create() {
+  rene = this.physics.add.image(32, 300, 'rene');
+  setReneConfig();
+  keys = this.input.keyboard.addKeys('Z,Q,S,D,SPACE');
+  meteor = this.physics.add.image(600, 300, 'meteor').setBounce(0.5, 0.5).setMass(10);
+  this.physics.add.collider(rene, meteor);
+
+  meteor.setAngularVelocity(20);
+  meteor.setVelocity(-200, 0);
+}
+
+function update() {
+  movingRene();
+}
+
+function setReneConfig() {
+  rene.setBounce(1, 1)
+    .setMass(1)
+    .setDrag(0.98, 0.98)
+    .setAngularVelocity(120)
+    .setMaxVelocity(300, 300);
+  rene.body.useDamping = true;
+}
+
+function movingRene() {
+  rene.setAccelerationX((keys.D.isDown - keys.Q.isDown) * 1000);
+  rene.setAccelerationY((keys.S.isDown - keys.Z.isDown) * 1000);
+  if (keys.S.isDown || keys.D.isDown || keys.Q.isDown || keys.Z.isDown) {
+    rene.setAngularVelocity(360);
+  } else {
+    rene.setAngularVelocity(120);
+  }
+}
